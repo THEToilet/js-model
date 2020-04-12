4e5a8b75a33ddf0da9423a12275cc102d708e821
THEToilet <bp18015@shibaura-it.ac.jp>
2020-04-12 20:45:29 +0900

    :pencil2: We can't add orbit.

--- js/cubeRotation.js
+++ js/cubeRotation.js
@@ -2,6 +2,8 @@ const scene = new THREE.Scene(); // シーンを作成
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
 
+// const controls = new THREE.OrbitControls(camera);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
@@ -15,8 +17,14 @@ const loader = new THREE.ColladaLoader();
 // dae ファイルのパスを指定
 loader.load('models/dae/simpleringblend.dae', (collada) => {
   const model = collada.scene;
+  model.rotation.x += getRadian(30);
+  model.rotation.y += getRadian(-15);
   scene.add(model); // 読み込み後に3D空間に追加
 });
+
+function getRadian(kakudo) {
+  return kakudo * Math.PI / 180;
+}
 
 function animate() {
   requestAnimationFrame( animate );
